bacbfbf754f8365e17d72c627185663447ca2d24
Sam Lau <liuyaozhu13hao@163.com>
2015-04-28 17:30:16 +0800

    create VideoPlayerView

---
+++ VideoPlayerView/VideoPlayerView.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayerView.h
+//  VideoPlayerView
+//
+//  Created by Sam Lau on 4/28/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import "SHPAbstractView.h"
+
+@interface VideoPlayerView : SHPAbstractView
+
+@end
---
+++ VideoPlayerView/VideoPlayerView.m
@@ -0,0 +1,25 @@
+//
+//  VideoPlayerView.m
+//  VideoPlayerView
+//
+//  Created by Sam Lau on 4/28/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import "VideoPlayerView.h"
+
+@implementation VideoPlayerView
+
+#pragma mark - Build view hierarchies
+- (void)addSubviews
+{
+    
+}
+
+#pragma mark - Define Layout
+- (void)defineLayout
+{
+    
+}
+
+@end
