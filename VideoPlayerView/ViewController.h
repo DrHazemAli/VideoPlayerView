b0044a4d30e636b36b252c5b3384fa3f3c322137
Sam Lau <liuyaozhu13hao@163.com>
2015-04-28 17:18:59 +0800

    create project

---
+++ VideoPlayerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerView
+//
+//  Created by Sam Lau on 4/28/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
